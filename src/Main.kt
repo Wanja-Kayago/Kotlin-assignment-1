34fbbdcc6560220b283209bd4f125446bf0835ab
Wanja-Kayago <kayagonaomiwanja@gmail.com>
2024-02-21 08:16:34 +0300

    Kotlin

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var name = "WanjaKayago"
+    println(name)
+    var age: Byte = 20
+    println(age)
+    var phoneNumber = "0741816757"
+    println(phoneNumber)
+    var weightInKg: Double = 65.67
+    var kenyan : Boolean = true
+    println(kenyan)
+    var nonkenyan : Boolean =false
+    println(nonkenyan)
+
+}
